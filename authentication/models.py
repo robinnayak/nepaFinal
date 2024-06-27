21e0ecf58c430b62135b32c16e9aac49694e6c98
robinnayak <robinnayak86@gmail.com>
2024-06-27 20:50:24 +0545

    added rating

--- authentication/models.py
+++ authentication/models.py
@@ -95,6 +95,7 @@ class Organization(models.Model):
     description = models.TextField(blank=True, null=True)   
     logo = models.ImageField(upload_to='organization_logo', blank=True, null=True)
     rating = models.DecimalField(max_digits=2, decimal_places=1, default=0.0)
+    earnings = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
     date_created = models.DateTimeField(auto_now_add=True)
     
     def __str__(self) -> str:
