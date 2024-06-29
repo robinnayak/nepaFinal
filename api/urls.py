e9e8ec369967cbc635862ddbf677348882ced735
robinnayak <robinnayak86@gmail.com>
2024-06-30 00:32:10 +0545

    url of passenger added

--- api/urls.py
+++ api/urls.py
@@ -2,12 +2,14 @@ from django.urls import path,include
 from . import views
 from authentication.views import  OrganizationDetailView,RegistrationView,LoginView,LogoutView,OrganizationView,DriverDetailView
 from organization.views import VehicleView
+
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('register/', RegistrationView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/',LogoutView.as_view(), name='logout'),
     path('organization/',include('organization.urls'), name='organizationss'),
+    path('passenger/',include('passenger.urls'), name='passenger'),
     # path('vehicle/',VehicleView.as_view(), name='vehicle' ),
     path('driver/<int:pk>/',include('driver.urls'), name='driver'),
 ]
