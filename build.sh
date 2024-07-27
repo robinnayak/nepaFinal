d422fd647fe463d4798844b6571eb67e41e0db6f
robinnayak <robinnayak86@gmail.com>
2024-07-27 19:20:25 +0545

    Fixed static files directory

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
  echo "BUILD START"
- python -m pip install -r requirements.txt
+ pip install -r requirements.txt
  python manage.py collectstatic --noinput --clear
  echo "BUILD END"
