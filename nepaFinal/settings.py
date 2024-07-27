5392b3e3939acf95a033d2278e9fddfc58529155
robinnayak <robinnayak86@gmail.com>
2024-07-27 18:57:15 +0545

    d

--- nepaFinal/settings.py
+++ nepaFinal/settings.py
@@ -101,8 +101,14 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
+
+STATIC_URL = '/static/'
+
+
+
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static" 
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
