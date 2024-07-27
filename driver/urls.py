811257108f2c1e7c692a7e8abb44819a2183d98c
robinnayak <robinnayak86@gmail.com>
2024-07-27 13:05:35 +0545

    error fixed for vercel

--- driver/urls.py
+++ driver/urls.py
@@ -2,12 +2,9 @@ from django.urls import path
 from . import views
 from authentication.views import  DriverDetailView
 urlpatterns = [
-<<<<<<< HEAD
-    path('',DriverDetailView.as_view(), name='drivers'),
-=======
     path('driver-related-organization/',views.DriverRelatedOrganizationView.as_view(),name="driver_related_organization"),
     path('push-alert/',views.PushAlertNotificationAPIView.as_view(),name='push_alert'),
->>>>>>> aa02306 (push notificaton updated)
+
 ]
 
 
